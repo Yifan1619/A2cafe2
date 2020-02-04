8249383aafeef82c9fb1c0db02af70700baa7c3c
Yifan1619 <50858490+Yifan1619@users.noreply.github.com>
2020-02-04 15:12:03 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+"use strict";
+
+function clearActive() {
+    for (var a in elements) for (var b in elements[a].classList) "active" === elements[a].classList[b] && elements[a].classList.remove("active");
+}
+
+function addActive(a) {
+    elements[a].classList.add("active");
+}
+
+function printSequence(a) {
+    for (var b = sequence[a], c = 0; c < b.length; c++) "1" === b[c] && addActive(c);
+}
+
+function stepSequence() {
+    clearActive(), counter >= sequence.length && (counter = 0), printSequence(counter), 
+    document.getElementById("display").innerHTML = sequence[counter], counter++, setTimeout(stepSequence, frequency);
+}
+
+var frequency = 1e3, sequence = [ '1101111','1110101','0100000','1011001','1101111','1101001','1111001' ], elements = document.getElementById("shape").getElementsByTagName("*"), counter = 0;
+
+window.onload = function() {
+    stepSequence();
+};
